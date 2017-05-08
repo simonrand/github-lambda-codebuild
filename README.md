645f72bdbebb4352241b636ad8fa244e7399ce09
Simon Rand <simonrand@gmail.com>
2017-05-08 15:44:24 +0100

    Initial commit of working version

---
+++ README.md
@@ -0,0 +1,8 @@
+Lambda function to trigger CodeBuild builds from GitHub, and report status back to GitHub.
+
+Full docs TBD:
+- Setup
+- IAM Roles
+- etc.
+
+Essentially forked from https://github.com/tatums/pull-request-builder.
---
+++ index.js
@@ -0,0 +1,39 @@
+'use strict'
+
+const build = require('./lib/build')
+const report = require('./lib/report')
+
+const branchesToExclude = ['master']
+const branchEnvironments = {
+  'master': 'production',
+  'staging': 'staging'
+}
+
+exports.handler = (event, context, callback) => {
+  const message = JSON.parse(event.Records[0].Sns.Message)
+
+  if(message && message.after) {
+    // Message from GitHub, building
+    const branch = message.ref.split('/').slice(-1)[0]
+
+    if(branchesToExclude.includes(branch)) return console.log(`Not building ${branch}, exiting.`)
+    if(message.deleted) return console.log('Branch deleted, exiting.')
+
+    build.run(message.after, branchEnvironments[branch])
+      .then(resp => {
+        callback(null, resp)
+      })
+      .catch(err => {
+        callback(err, null)
+      })
+  } else {
+    // Message from CodeBuild, reporting
+    report.run(message.buildId)
+      .then(resp => {
+        callback(null, resp);
+      })
+      .catch(err => {
+        callback(err, null);
+      })
+  }
+}
